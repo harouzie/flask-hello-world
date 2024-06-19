f92949ac51866b2bc1bd6fcb9b0b0ff45c6c2baa
harouzie <dophamquanghung@gmail.com>
2024-06-19 16:29:19 +0700

    disable flask main

--- api/index.py
+++ api/index.py
@@ -34,5 +34,5 @@ def qr_gen():
     return send_file(img_byte_arr, mimetype='image/png')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
