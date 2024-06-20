b2d513a488994074b79f3415f9c59a250e9e3392
harouzie <dophamquanghung@gmail.com>
2024-06-20 11:03:23 +0700

    qrcode param request

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, jsonify
 import io
 import base64
 import qrcode
+import json
 
 app = Flask(__name__)
 
@@ -13,8 +14,8 @@ def home():
 def about():
     return 'About'
 
-@app.route("/qrcode", methods=['GET'])
-def qr_gen():
+@app.route("/rickroll", methods=['GET'])
+def rickroll():
 
     youtube_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     qr = qrcode.QRCode(version=1, box_size=10)
@@ -36,5 +37,43 @@ def qr_gen():
     # return send_file(img_byte_arr, mimetype='image/png')
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route("/qrcode", methods=['POST'])
+def qr_gen():
+    # Extract the required parameters
+    # name = request.form.get("name")
+    # id = request.form.get("id")
+    # meeting_id = request.form.get("meeting_id")
+
+    name = request.args.get("name")
+    id = request.args.get("id")
+    meeting_id = request.args.get("meeting_id")
+
+    # Validate the parameters
+    # if not all([name, id, meeting_id]):
+    #     return jsonify({"error": "Missing required parameters"}), 400
+
+    # Create the QR code data
+    qr_data = {
+        "name": name,
+        # "id": id,
+        "meeting_id": "mid_01",
+        "command": "create_meeting"  # Replace with the actual command
+    }
+
+    # Encode the QR code data as JSON
+    json_data = json.dumps(qr_data)
+
+    qr = qrcode.QRCode(version=1, box_size=10)
+    qr.add_data(json_data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+    # base64_data = base64.b64encode(img_byte_arr.read()).decode()
+
+    return send_file(img_byte_arr, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
